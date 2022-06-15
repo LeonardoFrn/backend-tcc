024a028a27b2c59dfe62474071b061d0ba9dadd2
Leonardo França <leo_cq_frn@hotmail.com>
2022-06-14 22:26:25 -0300

    adicionando endpoint para adicionar temperatura

--- api/controllers/temperatura/index.js
+++ api/controllers/temperatura/index.js
@@ -12,6 +12,17 @@ router.get('/list', async (req, res) => {
     }
 });
 
+router.get('/add/:value', async (req, res) => {
+    try {
+        const temp = req.params.value;
+        const data = {temperatura: temp}
+        const result = await Service.Temperatura.addContent(data);
+        return res.status(200).json({ message: 'OK', data: result })
+    } catch (error) {
+        return res.status(500).send(returnError(error))
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
         const result = await Service.Temperatura.addContent(req.body);
